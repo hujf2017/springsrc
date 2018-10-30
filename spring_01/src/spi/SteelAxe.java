3b534305273bbff946c08afdfbec8c38062417a8
hujf <952405831@qq.com>
2018-10-30 12:26:50 +0800

    new steelAXE

---
+++ spring_01/src/spi/SteelAxe.java
@@ -0,0 +1,13 @@
+package spi;
+
+import Interface.Axe;
+
+public class SteelAxe implements Axe{
+
+	@Override
+	public String chop() {
+		// TODO Auto-generated method stub
+		return "¸Ö¸«¿³²ñºÃ¿ì";
+	}
+
+}
